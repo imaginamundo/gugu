941e36428a9667f4ba9015e079a85ded98ad9c05
Dio <dio@Mac-Mini-08s-Mac-mini.local>
2016-11-04 14:26:21 -0200

    Teste de velocidade e data

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ var app = express();
 var db;
 var mongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
-// var mongoUrl = process.env.MONGO_URL;
-var mongoUrl = 'mongodb://imaginamundo-secure:senha12345@ds041526.mlab.com:41526/gugu';
+var mongoUrl = process.env.MONGO_URL;
+// var mongoUrl = 'mongodb://imaginamundo-secure:senha12345@ds041526.mlab.com:41526/gugu';
 
 // Static
 app.use('/css', express.static(__dirname + '/css'));
@@ -34,43 +34,29 @@ function isInt(n) {
 }
 // Get gugu
 app.get('/list-gugu', (req, res) => {
-    mongoClient.connect(mongoUrl, (err, database) => {
-        // if (err) return console.log(err);
-        db = database;
-        var limit = 60;
-        if (req.query.limit && isInt(req.query.limit)) {
-            limit = req.query.limit;
-        }
-        
-        var page = 1;
-        if (req.query.page && isInt(req.query.page)) {
-            page = req.query.page;
-        }
+    
+    var limit = 60;
+    if (req.query.limit && isInt(req.query.limit)) {
+        limit = req.query.limit;
+    }
+    
+    var page = 1;
+    if (req.query.page && isInt(req.query.page)) {
+        page = req.query.page;
+    }
 
-        var skip = 0;
-        if (page > 1) {
-            skip = (page - 1) * limit;
-        }
+    var skip = 0;
+    if (page > 1) {
+        skip = (page - 1) * limit;
+    }
 
-        // console.log(req.query.page + ' ' + req.query.limit);
+    db.collection('gugu').find().sort({ _id: -1 }).limit(limit).skip(skip).toArray(function(erro, itens){
 
-        var findGugu = function(db, callback) {
-            db.collection('gugu').find().sort({ _id: -1 }).limit(limit).skip(skip).toArray(function(erro, itens){
-                
-
-                // db.getCollection('gugu').find().limit(50) futura paginação :D
-                // db.getCollection('gugu').
-                res.json(itens);
-            });
-        };
-
-        mongoClient.connect(mongoUrl, function(err, db) {
-            assert.equal(null, err);
-            findGugu(db, function() {
-                db.close();
-            });
-        });
+        // db.getCollection('gugu').find().limit(50) futura paginação :D
+        // db.getCollection('gugu').
+        res.json(itens);
     });
+    
 });
 
 // Post
@@ -120,6 +106,13 @@ app.post('/post-gugu', function (req, res) {
 // Server
 var port = process.env.PORT || 8080;
 
-app.listen(port, function () {
-    console.log('Example app listening on port ' + port + '!');
+mongoClient.connect(mongoUrl, function(err, database) {
+    if(err) throw err;
+
+    db = database;
+
+    app.listen(port, function () {
+        console.log('Example app listening on port ' + port + '!');
+    });
+
 });
